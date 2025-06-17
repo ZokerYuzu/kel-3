844b6cca8404655075164431c75cbedeb29819e1
ZokerYuzu <zokeryudha00@gmail.com>
2025-06-17 17:40:46 +0700

    Penambahan SplitMenuButtonApp By Yudha Pratama

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.control.*
+import javafx.scene.layout.StackPane
+import javafx.stage.Stage
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+class SplitMenuButtonApp : Application() {
+    override fun start(primaryStage: Stage) {
+        // Buat SplitMenuButton
+        val splitMenuButton = SplitMenuButton()
+        splitMenuButton.text = "Pilih Opsi"
+
+        // Tambahkan item ke SplitMenuButton
+        val item1 = MenuItem("Opsi 1")
+        val item2 = MenuItem("Opsi 2")
+        val item3 = MenuItem("Opsi 3")
+
+        splitMenuButton.items.addAll(item1, item2, item3)
+
+        // Event handler untuk menu item
+        item1.setOnAction {
+            println("Opsi 1 dipilih")
+        }
+        item2.setOnAction {
+            println("Opsi 2 dipilih")
+        }
+        item3.setOnAction {
+            println("Opsi 3 dipilih")
+        }
+
+        // Event handler untuk tombol utama (bukan menu)
+        splitMenuButton.setOnAction {
+            println("Tombol utama diklik")
+        }
+
+        // Tambahkan ke layout
+        val root = StackPane(splitMenuButton)
+        val scene = Scene(root, 300.0, 200.0)
+
+        primaryStage.title = "Contoh SplitMenuButton"
+        primaryStage.scene = scene
+        primaryStage.show()
     }
+}
+
+fun main() {
+    Application.launch(SplitMenuButtonApp::class.java)
 }
